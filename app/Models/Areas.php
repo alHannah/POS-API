fb773ee170c125d1bca5ab6e34aaf593ad4e1252
Kenneth <@kennethmazo123456>
2024-06-20 18:08:03 +0800

    [ADD] Added ORM to area_assignment,area, and store models

--- app/Models/Areas.php
+++ app/Models/Areas.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Areas extends Model
 {
@@ -15,8 +16,18 @@ class Areas extends Model
         'brand_id'
     ];
 
-    public function brand_per_area () : BelongsTo 
+    public function brand_per_area () : BelongsTo
     {
         return $this->belongsTo(Brands::class, 'brand_id');
+    }
+
+    public function areas_per_area_assignment() : HasMany
+    {
+        return $this->hasMany(AreaAssignment::class, 'area_id');
+    }
+
+    public function areas_per_stores() : HasMany
+    {
+        return $this->hasMany(Store::class, 'area_id');
     }
 }

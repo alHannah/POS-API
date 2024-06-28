3a2cfdece13b9b22c76a7ae715da7e4dd6239276
Kenneth <@kennethmazo123456>
2024-06-28 13:27:34 +0800

    [FIX] Schedule Group Controller

--- app/Http/Controllers/v1/web/dropdowns/DropdownController.php
+++ app/Http/Controllers/v1/web/dropdowns/DropdownController.php
@@ -9,22 +9,24 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
 use App\Models\PriceTier;
+use App\Models\Store;
 use App\Models\StoreGroup;
+use Illuminate\Support\Facades\Crypt;
 
 class DropdownController extends Controller
 {
-    public function brand_dropdown(Request $request) {
+    public function brand_dropdown(Request $request)
+    {
         try {
             DB::beginTransaction();
 
             $branddropdown = Brand::where('status', '1')->get();
 
             return response()->json([
-                "error"         =>false,
-                "message"       =>trans('messages.success'),
-                "data"          =>$branddropdown,
+                "error"         => false,
+                "message"       => trans('messages.success'),
+                "data"          => $branddropdown,
             ]);
-
         } catch (Exception $e) {
             DB::rollback();
             Log::info("Error: $e");
@@ -35,18 +37,18 @@ class DropdownController extends Controller
         }
     }
 
-    public function store_group_dropdown(Request $request) {
+    public function store_group_dropdown(Request $request)
+    {
         try {
             DB::beginTransaction();
 
             $storegroupdropdown = StoreGroup::get();
 
             return response()->json([
-                "error"         =>false,
-                "message"       =>trans('messages.success'),
-                "data"          =>$storegroupdropdown,
+                "error"         => false,
+                "message"       => trans('messages.success'),
+                "data"          => $storegroupdropdown,
             ]);
-
         } catch (Exception $e) {
             DB::rollback();
             Log::info("Error: $e");
@@ -57,30 +59,18 @@ class DropdownController extends Controller
         }
     }
 
-    public function stores_dropdown(Request $request) {
-        $brand = $this->brand_dropdown($request);
-        $storegroup = $this->store_group_dropdown($request);
-
-        return response()->json([
-            "error"             =>false,
-            "message"           =>trans('messages.success'),
-            "brand_dropdown"    =>$brand,
-            "store_dropdown"    =>$storegroup,
-            ]);
-        }
-
-    public function price_tier_dropdown(Request $request) {
+    public function price_tier_dropdown(Request $request)
+    {
         try {
             DB::beginTransaction();
 
             $priceTierDropdown = PriceTier::where('status', "active")->get();
 
             return response()->json([
-                "error"         =>false,
-                "message"       =>trans('messages.success'),
-                "data"          =>$priceTierDropdown,
+                "error"         => false,
+                "message"       => trans('messages.success'),
+                "data"          => $priceTierDropdown,
             ]);
-
         } catch (Exception $e) {
             DB::rollback();
             Log::info("Error: $e");
@@ -91,18 +81,18 @@ class DropdownController extends Controller
         }
     }
 
-    public function manager_dropdown(Request $request) {
+    public function manager_dropdown(Request $request)
+    {
         try {
             DB::beginTransaction();
 
             $managerDropdown = PriceTier::where('status', "active")->get();
 
             return response()->json([
-                "error"         =>false,
-                "message"       =>trans('messages.success'),
-                "data"          =>$managerDropdown,
+                "error"         => false,
+                "message"       => trans('messages.success'),
+                "data"          => $managerDropdown,
             ]);
-
         } catch (Exception $e) {
             DB::rollback();
             Log::info("Error: $e");
@@ -113,18 +103,46 @@ class DropdownController extends Controller
         }
     }
 
-    public function add_product_dropdown(Request $request) {
+    public function add_product_dropdown(Request $request)
+    {
         try {
             DB::beginTransaction();
 
             $managerDropdown = PriceTier::where('status', "active")->get();
 
             return response()->json([
-                "error"         =>false,
-                "message"       =>trans('messages.success'),
-                "data"          =>$managerDropdown,
+                "error"         => false,
+                "message"       => trans('messages.success'),
+                "data"          => $managerDropdown,
             ]);
+        } catch (Exception $e) {
+            DB::rollback();
+            Log::info("Error: $e");
+            return response()->json([
+                "error"         => true,
+                "message"       => trans("messages.error"),
+            ]);
+        }
+    }
 
+    public function stores_dropdown(Request $request)
+    {
+        try {
+            DB::beginTransaction();
+
+            $storeData = Store::where('status', 1)->get();
+
+            /* $storeDataEncryptedId = $storeData->map(function ($item){
+                $item->encryptedId = Crypt::encrypt($item->id);
+                return $item;
+            }); */
+
+            return response()->json([
+                "error"         => false,
+                "message"       => trans('messages.success'),
+                "data"          => $storeData,
+                //"data"          => $storeDataEncryptedId,
+            ]);
         } catch (Exception $e) {
             DB::rollback();
             Log::info("Error: $e");
